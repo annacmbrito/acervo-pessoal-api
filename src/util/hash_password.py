98500dea2ee4507792f0d1b3771509907d5d1af8
Anna Brito <annet.rj@gmail.com>
2024-05-01 15:27:44 -0300

    Add method to compare plaintext with hashed password

--- src/util/hash_password.py
+++ src/util/hash_password.py
@@ -4,3 +4,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password:str):
     return pwd_context.hash(password)
+
+def are_equals_passwords(plaintext: str, hashed: str):
+    return pwd_context.verify(plaintext, hashed)
