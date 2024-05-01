5d8fca22f158927541aa2260c3c2500ba05332de
Anna Brito <annet.rj@gmail.com>
2024-05-01 13:15:11 -0300

    Add validation in schema SaveUserRequest

--- src/routes/users/schemas.py
+++ src/routes/users/schemas.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 from src.routes.users.models import User
 
 class SaveUserRequest(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
+    first_name: str = Field(..., min_length=3, max_length=16, pattern="^[a-zA-Z]+$")
+    last_name: str = Field(..., min_length=3, max_length=16, pattern="^[a-zA-Z]+$")
+    email: EmailStr = Field(..., max_length=64)
+    password: str = Field(..., min_length=8, max_length=128, pattern="^[a-zA-Z0-9]*[a-zA-Z]+[0-9]+[a-zA-Z0-9]*$")
 
     def to_model(self):
         user = User()
