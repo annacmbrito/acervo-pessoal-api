6e8b876db6ea470faaa52bb98d8179d72d16be17
Anna Brito <annet.rj@gmail.com>
2024-05-01 15:55:46 -0300

    Return LoginResponse instead raw string

--- src/routes/auth/services.py
+++ src/routes/auth/services.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from src.routes.auth.schemas import LoginRequest
+from src.routes.auth.schemas import *
 from src.routes.commons.jwt_service import jwt
 from src.routes.users.services import UserService
 from src.util.hash_password import are_equals_passwords
@@ -14,7 +14,7 @@ class AuthService:
             user = self.user_service.find_by_email(request.email)
             if(not are_equals_passwords(request.password, user.password)):
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-            return jwt.generate_token(user.email)
+            return LoginResponse(token=jwt.generate_token(user.email))
         except HTTPException:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
