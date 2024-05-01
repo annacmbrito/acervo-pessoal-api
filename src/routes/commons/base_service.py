3c4ad9e6aceccc459d6c2454c14935cf45669965
Anna Brito <annet.rj@gmail.com>
2024-05-01 00:24:38 -0300

    Add BaseService

---
+++ src/routes/commons/base_service.py
@@ -0,0 +1,17 @@
+from typing import Any, Type
+from sqlalchemy.orm import Session
+from src.config.database import Base
+
+class BaseService:
+    def __init__(self, session: Session, type: Type[Base]):
+        self.session = session
+        self.type = type
+
+    def save(self, model: Any):
+        self.session.add(model)
+        self.session.commit()
+        self.session.refresh(model)
+        return model
+    
+    def exists(self, condition: Any): 
+        return self.session.query(self.type).filter(condition).first() is not None
