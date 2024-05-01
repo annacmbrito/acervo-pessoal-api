7c4158443a3f2ab0bc0af2cc7c1ea937d415e575
Anna Brito <annet.rj@gmail.com>
2024-05-01 15:47:09 -0300

    Add method to authenticate user

--- src/routes/auth/services.py
+++ src/routes/auth/services.py
@@ -1,7 +1,20 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
+from src.routes.auth.schemas import LoginRequest
+from src.routes.commons.jwt_service import jwt
 from src.routes.users.services import UserService
+from src.util.hash_password import are_equals_passwords
 
 class AuthService:
     def __init__(self, session: Session):
         self.user_service = UserService(session)
+
+    def authenticate(self, request: LoginRequest):
+        try:
+            user = self.user_service.find_by_email(request.email)
+            if(not are_equals_passwords(request.password, user.password)):
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+            return jwt.generate_token(user.email)
+        except HTTPException:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
