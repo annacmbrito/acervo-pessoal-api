01290c9a3c3545d139ed9e37060639ada8e82c7b
Anna Brito <annet.rj@gmail.com>
2024-05-01 00:26:22 -0300

    Add method to save user

--- src/routes/users/services.py
+++ src/routes/users/services.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from src.routes.commons.base_service import BaseService
 from src.routes.users.models import User
@@ -5,3 +6,8 @@ from src.routes.users.models import User
 class UserService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, User)
+
+    def save(self, user: User):
+        if(super().exists(User.email == user.email)):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already exists")
+        return super().save(user)
