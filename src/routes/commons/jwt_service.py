60b49a4d1f9aeaf7d6daef141ce4c6d23e3ab3ed
Anna Brito <annet.rj@gmail.com>
2024-05-01 15:05:23 -0300

    Add method to generate token jwt

--- src/routes/commons/jwt_service.py
+++ src/routes/commons/jwt_service.py
@@ -1,9 +1,22 @@
+from jose import jwt as jwt_jose
+
 import os
+import time
 
 class JwtService:
     JWT_SECRET = os.getenv('JWT_SECRET')
     JWT_EXPIRATION_IN_MILLISECONDS = int(os.getenv('JWT_EXPIRATION_IN_MILLISECONDS'))
     USER_EMAIL_KEY: str = "email"
     EXPIRATION_KEY: str = "expiration"
+    
+    def generate_token(self, email: str):
+        return jwt_jose.encode(
+            key=self.JWT_SECRET, 
+            algorithm="HS256",
+            claims={
+                self.USER_EMAIL_KEY: email,
+                self.EXPIRATION_KEY: round(time.time() * 1000) + self.JWT_EXPIRATION_IN_MILLISECONDS,
+            }
+        )
 
 jwt = JwtService()
