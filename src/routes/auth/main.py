5a3c4f2136168f2fc3a22e94b1a56e43d9e61d77
Anna Brito <annet.rj@gmail.com>
2024-05-01 15:51:38 -0300

    Add route to authenticate user

--- src/routes/auth/main.py
+++ src/routes/auth/main.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.config.database import get_db_session
+from src.routes.auth.schemas import LoginRequest
+from src.routes.auth.services import AuthService
 
 router = APIRouter(
     prefix="/api/v1/auth",
     tags=["Authentication"]
 )
+
+@router.post("/")
+def authenticate(request: LoginRequest, session: Session = Depends(get_db_session)):
+    return AuthService(session).authenticate(request)
