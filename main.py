cf0dbfa71a8d0ba6bd6d1eebd5261b000f5a9297
Anna Brito <annet.rj@gmail.com>
2024-05-01 15:52:10 -0300

    Include auth router in app

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ load_dotenv()
 
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
+from src.routes.auth.main import router as auth_router
 from src.routes.users.main import router as users_router
 
 app = FastAPI()
+app.include_router(auth_router)
 app.include_router(users_router)
 
 @app.get("/")
