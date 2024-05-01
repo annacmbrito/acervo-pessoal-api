4c4be33caf4042658bcf58f46fea45e131fbf9b2
Anna Brito <annet.rj@gmail.com>
2024-04-30 23:52:10 -0300

    Include users router to api

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ load_dotenv()
 
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
+from src.routes.users.main import router as users_router
 
 app = FastAPI()
+app.include_router(users_router)
 
 @app.get("/")
 def main_route():
