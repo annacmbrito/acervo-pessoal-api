4669ef26b8dd26fd8a7f17b96756e88e79436e1f
Anna Brito <annet.rj@gmail.com>
2024-05-02 22:53:32 -0300

    Adding CORS config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 from src.routes.auth.main import router as auth_router
 from src.routes.users.main import router as users_router
@@ -10,6 +11,14 @@ from src.routes.users.main import router as users_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(users_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main_route():
