0d2acb060c684d7511075fb158fc1bc3fe3ebe51
Anna Brito <annet.rj@gmail.com>
2024-05-02 22:10:55 -0300

    Add method to protect authenticated routes

--- src/routes/auth/main.py
+++ src/routes/auth/main.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from src.config.database import get_db_session
 from src.routes.auth.schemas import LoginRequest
 from src.routes.auth.services import AuthService
+from src.routes.commons.jwt_service import jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 router = APIRouter(
     prefix="/api/v1/auth",
     tags=["Authentication"]
 )
 
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        return jwt.get_user_email(token)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=e.args[0])
+
 @router.post("/")
 def authenticate(request: LoginRequest, session: Session = Depends(get_db_session)):
     return AuthService(session).authenticate(request)
+
