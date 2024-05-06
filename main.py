3bf65a536e14307eeaaa7a4984dcbfc50fdc4246
Anna Brito <annet.rj@gmail.com>
2024-05-06 20:40:52 -0300

    Add books router in app

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 from src.routes.auth.main import router as auth_router
+from src.routes.books.main import router as books_router
 from src.routes.users.main import router as users_router
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(books_router)
 app.include_router(users_router)
 
 app.add_middleware(
