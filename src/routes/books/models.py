c0638795f09e0d512f2b274dcb5f70096c472b96
Anna Brito <annet.rj@gmail.com>
2024-05-06 19:45:17 -0300

    Add model subcategory

--- src/routes/books/models.py
+++ src/routes/books/models.py
@@ -1,5 +1,6 @@
 from src.config.database import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, ForeignKey, String, Integer
+from sqlalchemy.orm import relationship
 
 class Author(Base):
     __tablename__ = 'author'
@@ -18,3 +19,14 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(length=32))
+
+    subcategories = relationship("Subcategory", back_populates="category")
+
+class Subcategory(Base):
+    __tablename__ = 'subcategory'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=32))
+    category_id = Column(Integer, ForeignKey('category.id'))
+
+    category = relationship("Category", back_populates="subcategories")
