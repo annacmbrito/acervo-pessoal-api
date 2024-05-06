f48f3947933c94173e46916ff8d7bed1d30d7fd0
Anna Brito <annet.rj@gmail.com>
2024-05-06 20:21:18 -0300

    Add book service

---
+++ src/routes/books/services.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import Session
+from src.routes.commons.base_service import BaseService
+from src.routes.books.models import Book
+
+class BookService(BaseService):
+    def __init__(self, session: Session):
+        super().__init__(session, Book)
