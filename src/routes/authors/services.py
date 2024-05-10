41e7417e27e988dc7ca7d151043d16e18b8fea26
Anna Brito <annet.rj@gmail.com>
2024-05-10 02:38:46 -0300

    Paginate find all authors method

--- src/routes/authors/services.py
+++ src/routes/authors/services.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.authors.models import Author
 
@@ -6,8 +7,8 @@ class AuthorService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Author)
 
-    def find_all(self):
-        return super().find_all()
+    def find_all(self, page: Page):
+        return super().find_all(page)
     
     def save(self, author: Author):
         return super().save(author)
