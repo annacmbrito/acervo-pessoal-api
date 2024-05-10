5a47df4432620e082b3b5d705c989fd87d9f01ca
Anna Brito <annet.rj@gmail.com>
2024-05-10 03:45:04 -0300

    Organize code

--- src/routes/commons/base_service.py
+++ src/routes/commons/base_service.py
@@ -21,11 +21,13 @@ class BaseService:
         return self.session.query(self.type).filter(condition)
     
     def find_all(self, page: Page):
-        order_column = getattr(self.type, page.sort_by)
-        query_order = order_column.asc() if page.order == 'ASC' else order_column.desc()
-        result = self.session.query(self.type).order_by(query_order)
+        result = self.session.query(self.type)
         page.number_of_elements = result.count()
-        if(page.size >= 0):
+        if page.sort_by is not None:
+            order_column = getattr(self.type, page.sort_by)
+            query_order = order_column.asc() if page.order == 'ASC' else order_column.desc()
+            result = result.order_by(query_order)
+        if page.size >= 0:
             result = result.limit(page.size).offset(page.size * page.offset)
         page.content = result.all()
         return page.model_dump()
