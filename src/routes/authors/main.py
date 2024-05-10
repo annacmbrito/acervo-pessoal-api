9d59e0b4622f0d6d26d6fe7c412993919e6f5743
Anna Brito <annet.rj@gmail.com>
2024-05-10 02:14:05 -0300

    Add route to save new author

--- src/routes/authors/main.py
+++ src/routes/authors/main.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from src.config.database import get_db_session
+from src.routes.authors.schemas import SaveAuthorRequest
+from src.routes.authors.services import AuthorService
 
 router = APIRouter(
     prefix="/api/v1/authors", 
     tags=["Authors"],
     responses={ 404: { "description": "Not found" } }
 )
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def save_user(request: SaveAuthorRequest, session: Session = Depends(get_db_session)):
+    return AuthorService(session).save(request.to_model())
