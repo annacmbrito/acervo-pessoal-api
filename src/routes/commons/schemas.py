f2d72aa3acb94eec980b15a8fbd023ad9e2f5e2e
Anna Brito <annet.rj@gmail.com>
2024-05-10 03:47:45 -0300

    Add validation in page schema

--- src/routes/commons/schemas.py
+++ src/routes/commons/schemas.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Any
+from enum import Enum
+
+class OrderDirection(Enum):
+    ASC = 'ASC'
+    DESC = 'DESC'
 
 class Page(BaseModel):
     size:int
-    offset:int
+    offset:int = Field(0, ge=0)
     sort_by:str
-    order: str
+    order: OrderDirection
     number_of_elements: int = 0
     content:List[Any] = []
