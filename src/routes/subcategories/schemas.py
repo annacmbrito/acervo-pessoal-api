bfffbf278be89833afa5b71c92c5124403475d2d
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:57:18 -0300

    Add schema to represent subcategory query filter

--- src/routes/subcategories/schemas.py
+++ src/routes/subcategories/schemas.py
@@ -10,3 +10,15 @@ class SaveSubcategoryRequest(BaseModel):
         subcategory.name = self.name
         subcategory.category_id = self.category_id
         return subcategory
+    
+class SubcategoryFilter(BaseModel):
+    name: str | None = Field(None, max_length=32)
+    category_id: int | None = Field(None, ge=1, alias="categoryId")
+
+    def condition(self):
+        conditions = []
+        if self.name is not None:
+            conditions.append(Subcategory.name.contains(self.name))
+        if self.category_id is not None:
+            conditions.append(Subcategory.category_id == self.category_id)
+        return conditions
