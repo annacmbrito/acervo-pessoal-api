31e54b781dfeb26640b6a517d942bd5e6e2a30e0
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:06:39 -0300

    Add method to find all categories

--- src/routes/categories/main.py
+++ src/routes/categories/main.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
+from src.config.database import get_db_session
+from src.routes.categories.service import CategoryService
 
 router = APIRouter(
     prefix="/api/v1/categories", 
     tags=["Categories"],
     responses={ 404: { "description": "Not found" } }
 )
+
+@router.get("/")
+def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
+    return CategoryService(session).find_all(page)
