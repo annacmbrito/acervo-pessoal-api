259a69849ebe8159a8de54d8984cbd6a6cabeb36
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:22:35 -0300

    Add route to find all subcategories

--- src/routes/subcategories/main.py
+++ src/routes/subcategories/main.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
+from src.config.database import get_db_session
+from src.routes.subcategories.service import SubcategoryService
 
 router = APIRouter(
     prefix="/api/v1/subcategories", 
     tags=["Subcategories"],
     responses={ 404: { "description": "Not found" } }
 )
+
+@router.get("/")
+def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
+    return SubcategoryService(session).find_all(page)
