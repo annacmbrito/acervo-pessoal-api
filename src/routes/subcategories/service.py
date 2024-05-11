d699842303f73463222bcef0faf56fcd7826787d
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:20:12 -0300

    Add method to find all subcategories

--- src/routes/subcategories/service.py
+++ src/routes/subcategories/service.py
@@ -1,10 +1,14 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.subcategories.models import Subcategory
 
 class SubcategoryService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Subcategory)
+
+    def find_all(self, page: Page):
+        return super().find_all(page)
 
     def save(self, book: Subcategory):
         return super().save(book)
