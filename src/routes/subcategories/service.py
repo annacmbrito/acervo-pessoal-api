92dc76b2eddaaf0fc1bb16eaa39dca98b1eb0f5a
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:57:46 -0300

    Add filter in find all subcategories method

--- src/routes/subcategories/service.py
+++ src/routes/subcategories/service.py
@@ -2,13 +2,14 @@ from sqlalchemy.orm import Session
 from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.subcategories.models import Subcategory
+from src.routes.subcategories.schemas import SubcategoryFilter
 
 class SubcategoryService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Subcategory)
 
-    def find_all(self, page: Page):
-        return super().find_all(page)
+    def find_all(self, filter: SubcategoryFilter, page: Page):
+        return super().find_all(page, filter.condition())
 
     def save(self, book: Subcategory):
         return super().save(book)
