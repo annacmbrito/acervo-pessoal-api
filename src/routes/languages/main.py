a0514086d1a5aa1d7f87fcf8c2b2c3361a547d36
Anna Brito <annet.rj@gmail.com>
2024-05-11 12:39:04 -0300

    Add route to save language

--- src/routes/languages/main.py
+++ src/routes/languages/main.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from src.routes.commons.schemas import Page
 from src.config.database import get_db_session
+from src.routes.languages.schemas import SaveLanguageRequest
 from src.routes.languages.service import LanguageService
 
 router = APIRouter(
@@ -13,3 +14,7 @@ router = APIRouter(
 @router.get("/")
 def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
     return LanguageService(session).find_all(page)
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def save_user(request: SaveLanguageRequest, session: Session = Depends(get_db_session)):
+    return LanguageService(session).save(request.to_model())
