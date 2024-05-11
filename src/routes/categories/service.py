f38acf00da6a0b885cbfead85ba3a7c9dcd30d0e
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:02:07 -0300

    Add method to find all categories

--- src/routes/categories/service.py
+++ src/routes/categories/service.py
@@ -1,7 +1,11 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.categories.models import Category
 
 class CategoryService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Category)
+
+    def find_all(self, page: Page):
+        return super().find_all(page)
