b53a21c1344313990571ec2e6694803322eddfac
Anna Brito <annet.rj@gmail.com>
2024-05-11 13:22:54 -0300

    Add route to save new subcategory

--- src/routes/subcategories/main.py
+++ src/routes/subcategories/main.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from src.routes.commons.schemas import Page
 from src.config.database import get_db_session
+from src.routes.subcategories.schemas import SaveSubcategoryRequest
 from src.routes.subcategories.service import SubcategoryService
 
 router = APIRouter(
@@ -13,3 +14,7 @@ router = APIRouter(
 @router.get("/")
 def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
     return SubcategoryService(session).find_all(page)
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def save_user(request: SaveSubcategoryRequest, session: Session = Depends(get_db_session)):
+    return SubcategoryService(session).save(request.to_model())
