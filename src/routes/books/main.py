b7ba411e21f3abd66cbc0a12b12b891004936289
Anna Brito <annet.rj@gmail.com>
2024-05-11 14:40:57 -0300

    Add route to get all books

--- src/routes/books/main.py
+++ src/routes/books/main.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.config.database import get_db_session
 from src.routes.books.schemas import SaveBookRequest
 from src.routes.books.service import BookService
@@ -9,6 +10,10 @@ router = APIRouter(
     tags=["Books"],
     responses={ 404: { "description": "Not found" } }
 )
+
+@router.get("/")
+def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
+    return BookService(session).find_all(page)
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def save_user(request: SaveBookRequest, session: Session = Depends(get_db_session)):
