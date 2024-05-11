c512d13449c839d1bd1d387cd6228f52b9c7d56c
Anna Brito <annet.rj@gmail.com>
2024-05-11 12:38:19 -0300

    Add route to get all languages

--- src/routes/languages/main.py
+++ src/routes/languages/main.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
+from src.config.database import get_db_session
+from src.routes.languages.service import LanguageService
 
 router = APIRouter(
     prefix="/api/v1/languages", 
     tags=["Languages"],
     responses={ 404: { "description": "Not found" } }
 )
+
+@router.get("/")
+def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
+    return LanguageService(session).find_all(page)
