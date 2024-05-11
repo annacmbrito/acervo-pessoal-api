cfe894f88a81379939a9c1fea7c0fe48d70e748d
Anna Brito <annet.rj@gmail.com>
2024-05-11 12:35:14 -0300

    Add method to find all languages

--- src/routes/languages/service.py
+++ src/routes/languages/service.py
@@ -1,10 +1,14 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.languages.models import Language
 
 class LanguageService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Language)
+
+    def find_all(self, page: Page):
+        return super().find_all(page)
 
     def save(self, language: Language):
         return super().save(language)
