f020fc979376f4cbc8ad91f20467ca3ba768423e
Anna Brito <annet.rj@gmail.com>
2024-05-11 14:39:56 -0300

    Add method to find all books

--- src/routes/books/service.py
+++ src/routes/books/service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.books.models import Book
 
@@ -8,3 +9,6 @@ class BookService(BaseService):
 
     def save(self, book: Book):
         return super().save(book)
+
+    def find_all(self, page: Page):
+        return super().find_all(page)
