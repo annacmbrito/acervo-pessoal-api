663970ee83efffd63b1fb262ed6033fd9d8ee103
Anna Brito <annet.rj@gmail.com>
2024-05-11 12:59:26 -0300

    Move category model to categories module

--- src/routes/books/models.py
+++ src/routes/books/models.py
@@ -2,15 +2,6 @@ from src.config.database import Base
 from sqlalchemy import Column, ForeignKey, String, Integer, Boolean
 from sqlalchemy.orm import relationship
 
-class Category(Base):
-    __tablename__ = 'category'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(length=32))
-
-    books = relationship("Book", back_populates="category")
-    subcategories = relationship("Subcategory", back_populates="category")
-
 class Subcategory(Base):
     __tablename__ = 'subcategory'
 
---
+++ src/routes/categories/models.py
@@ -0,0 +1,12 @@
+from src.config.database import Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=32))
+
+    books = relationship("Book", back_populates="category")
+    subcategories = relationship("Subcategory", back_populates="category")
