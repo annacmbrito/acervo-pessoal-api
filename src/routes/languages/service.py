79265d821618c458e311b6de72b742598b2ea816
Anna Brito <annet.rj@gmail.com>
2024-05-11 12:34:10 -0300

    Add language service

---
+++ src/routes/languages/service.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import Session
+from src.routes.commons.base_service import BaseService
+from src.routes.languages.models import Language
+
+class LanguageService(BaseService):
+    def __init__(self, session: Session):
+        super().__init__(session, Language)
