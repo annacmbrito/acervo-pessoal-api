296a5058ea2ddf61cc2b63ff0af608d9376444a7
Anna Brito <annet.rj@gmail.com>
2024-05-14 01:08:18 -0300

    Add route to get all publishers

--- src/routes/publishers/main.py
+++ src/routes/publishers/main.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
+from src.config.database import get_db_session
+from src.routes.publishers.service import PublisherService
 
 router = APIRouter(
     prefix="/api/v1/publishers", 
     tags=["Publishers"],
     responses={ 404: { "description": "Not found" } }
 )
+
+@router.get("/")
+def find_all(page: Page = Depends(), session: Session = Depends(get_db_session)):
+    return PublisherService(session).find_all(page)
