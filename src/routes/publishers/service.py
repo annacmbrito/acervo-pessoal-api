1182df6ede7faa2d7b52a9594d619b3934c71cf8
Anna Brito <annet.rj@gmail.com>
2024-05-14 01:06:29 -0300

    Add method to find all publishers

--- src/routes/publishers/service.py
+++ src/routes/publishers/service.py
@@ -1,7 +1,11 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.publishers.models import Publisher
 
 class PublisherService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Publisher)
+
+    def find_all(self, page: Page):
+        return super().find_all(page)
