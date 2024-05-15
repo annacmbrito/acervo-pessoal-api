cd0977358a0aa3717cdf13afe0b4fba51e1b9e7b
Anna Brito <annet.rj@gmail.com>
2024-05-14 22:22:12 -0300

    Change in book table columns schema

--- migrations/20240506_06_TlqHJ-create-table-book.sql
+++ migrations/20240506_06_TlqHJ-create-table-book.sql
@@ -4,13 +4,15 @@
 CREATE TABLE IF NOT EXISTS book
 (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(32) NOT NULL,
+    name VARCHAR(128) NOT NULL,
 	description TEXT NOT NULL,
 	comment TEXT NOT NULL,
 	rating INT NOT NULL,
-	available BOOLEAN NOT NULL,
+	pages INT NOT NULL,
+	status VARCHAR(32) NOT NULL,
 	author_id INT NOT NULL REFERENCES author(id),
 	language_id INT NOT NULL REFERENCES language(id),
+	publisher_id INT NOT NULL REFERENCES publisher(id),
 	category_id INT NOT NULL REFERENCES category(id),
 	subcategory_id INT NOT NULL REFERENCES subcategory(id)
 );
