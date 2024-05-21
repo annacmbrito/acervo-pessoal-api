55aed821ec15df2096266878388e674e8cce3a7e
Anna Brito <annet.rj@gmail.com>
2024-05-20 21:35:23 -0300

    Make save book request fields optional

--- src/routes/books/schemas.py
+++ src/routes/books/schemas.py
@@ -10,16 +10,16 @@ class BookStatus(Enum):
 
 class SaveBookRequest(BaseModel):
     name: str = Field(..., min_length=3, max_length=128)
-    description: str = Field('')
-    comment: str = Field('')
-    pages: int = Field(0, ge=0)
+    description: str | None = Field(None)
+    comment: str | None = Field(None)
+    pages: int | None = Field(None, ge=0)
     rating: int = Field(0, ge=0, le=5)
     status: BookStatus = Field(BookStatus.AVAILABLE)
-    author: str = Field(..., min_length=3, max_length=32)
-    language: str = Field(..., min_length=3, max_length=32)
-    publisher: str = Field(..., min_length=3, max_length=32)
-    category: str = Field(..., min_length=3, max_length=32)
-    subcategory: str = Field(..., min_length=3, max_length=32)
+    author: str | None = Field(None, min_length=3, max_length=32)
+    language: str | None = Field(None, min_length=3, max_length=32)
+    publisher: str | None = Field(None, min_length=3, max_length=32)
+    category: str | None = Field(None, min_length=3, max_length=32)
+    subcategory: str | None = Field(None, min_length=3, max_length=32)
 
     def to_model(self):
         book = Book()
