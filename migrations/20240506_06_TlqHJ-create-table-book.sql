2a17369a136e19abfae92e973ebe60210ee1641e
Anna Brito <annet.rj@gmail.com>
2024-05-20 21:33:06 -0300

    Remove not null constraint from columns in book table

--- migrations/20240506_06_TlqHJ-create-table-book.sql
+++ migrations/20240506_06_TlqHJ-create-table-book.sql
@@ -5,14 +5,14 @@ CREATE TABLE IF NOT EXISTS book
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
-	description TEXT NOT NULL,
-	comment TEXT NOT NULL,
-	rating INT NOT NULL,
-	pages INT NOT NULL,
-	status VARCHAR(32) NOT NULL,
-	author_id INT NOT NULL REFERENCES author(id),
-	language_id INT NOT NULL REFERENCES language(id),
-	publisher_id INT NOT NULL REFERENCES publisher(id),
-	category_id INT NOT NULL REFERENCES category(id),
-	subcategory_id INT NOT NULL REFERENCES subcategory(id)
+	description TEXT,
+	comment TEXT,
+	rating INT,
+	pages INT,
+	status VARCHAR(32),
+	author_id INT REFERENCES author(id),
+	language_id INT REFERENCES language(id),
+	publisher_id INT REFERENCES publisher(id),
+	category_id INT REFERENCES category(id),
+	subcategory_id INT REFERENCES subcategory(id)
 );
