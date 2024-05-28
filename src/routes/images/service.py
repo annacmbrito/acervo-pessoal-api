f7e300940797a951582f69378ce9d8d2945c6134
Anna Brito <annet.rj@gmail.com>
2024-05-28 20:42:30 -0300

    Add method to save image

--- src/routes/images/service.py
+++ src/routes/images/service.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import Session
+from src.routes.commons.schemas import Page
 from src.routes.commons.base_service import BaseService
 from src.routes.images.models import Image
 
 class ImageService(BaseService):
     def __init__(self, session: Session):
         super().__init__(session, Image)
+
+    def find_all(self, page: Page):
+        return super().find_all(page)
+
+    def save(self, image: Image):
+        return super().save(image)
