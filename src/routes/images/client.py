b6468b43731fbe369b6afa897f8343cf9c065ee4
Anna Brito <annet.rj@gmail.com>
2024-05-28 22:34:17 -0300

    Add method to upload image to imagekit

--- src/routes/images/client.py
+++ src/routes/images/client.py
@@ -1,6 +1,10 @@
 import os
+import base64
 
 from imagekitio import ImageKit
+from imagekitio.models.results import UploadFileResult
+from fastapi import UploadFile, HTTPException
+from src.routes.images.models import Image
 
 class ImageKitClient:
     def __init__(self):
@@ -9,3 +13,27 @@ class ImageKitClient:
             private_key=os.getenv("IMAGE_KIT_PRIVATE_KEY"),
             url_endpoint=os.getenv("IMAGE_KIT_ENDPOINT")
         )
+
+    async def upload(self, file: UploadFile):
+        try:
+            file_content = await file.read()
+
+            if not file_content:
+                raise HTTPException(status_code=400, detail="File content is empty")
+
+            upload_response = self.client.upload(
+                file=base64.b64encode(file_content),
+                file_name=file.filename
+            )
+
+            return self.convert_to_image(upload_response)
+        except HTTPException as e:
+            raise e
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {str(e)}")
+    
+    def convert_to_image(self, upload_response: UploadFileResult):
+        image = Image()
+        image.id = upload_response.file_id
+        image.url = upload_response.url
+        return image
